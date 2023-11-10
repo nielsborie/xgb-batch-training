e8c667afcfb3b4dc1da88df5c1774905760b81a4
Borie, Niels <niels.borie@bleckwen.ai>
2023-11-10 19:15:14 +0100

    feat: use randomSplit to get each batch data in order to improve perf issues

--- src/modeling/training/model_training_task.py
+++ src/modeling/training/model_training_task.py
@@ -5,44 +5,12 @@ from typing import List, Dict
 
 import xgboost as xgb
 from pyspark.sql import DataFrame
-from pyspark.sql import Window
 from pyspark.sql import functions as F
 
 from src import models_dir
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-
-def add_batch_column(df: DataFrame, batch_size: int) -> DataFrame:
-    """
-    Add a 'batch' column to the DataFrame based on the batch size.
-
-    Args:
-    df (DataFrame): DataFrame to process.
-    batch_size (int): Size of each batch.
-
-    Returns:
-    DataFrame: DataFrame with an added 'batch' column.
-
-    Raises:
-    ValueError: If batch_size is not a positive integer.
-    """
-    if not isinstance(batch_size, int) or batch_size <= 0:
-        raise ValueError("batch_size should be a positive integer.")
-
-    logger.debug(f"Adding 'batch' column with a batch size of {batch_size}...")
-
-    # Create a window specification for ordering by monotonically increasing id
-    window_spec = Window.orderBy(F.monotonically_increasing_id())
-
-    # Add a 'batch' column based on the batch size
-    df = df.withColumn("batch", (F.row_number().over(window_spec) - 1) / batch_size)
-    df = df.withColumn("batch", F.floor("batch"))
-
-    logger.debug("Successfully added the 'batch' column.")
-    return df
-
 
 def model_training_task(experiment_id: str,
                         train_data: DataFrame,
@@ -84,46 +52,45 @@ def model_training_task(experiment_id: str,
     if batch_size <= 0:
         raise ValueError("Batch size should be a positive integer.")
 
-    start_time = time.time()
-    num_rows = train_data.count()
-    total_batches = num_rows // batch_size + 1
-    batch_count = 0
-    train_data = add_batch_column(df=train_data, batch_size=batch_size)
-
     logger.info(f"Experiment ID: {experiment_id} - Starting [model_training] task ...")
+    start_time = time.time()
     xgb_model = None
-    for i in range(total_batches):
+    num_rows = train_data.count()
+    num_batches = (num_rows + batch_size - 1) // batch_size
+    splits = train_data.randomSplit([1.0] * num_batches, seed=42)
+    total_batches = len(splits)
+    batch_count = 0
+    for i, batch_train_data in enumerate(splits):
         batch_start_time = time.time()
         current_batch = i + 1
         remaining_batches = total_batches - current_batch
-        batch_train_data = train_data.filter(train_data["batch"] == i)
-        batch_size_current = batch_train_data.count()
 
-        logger.debug(f"Processing Batch {i + 1} - Size: {batch_size_current}")
+        batch_size_current = batch_train_data.count()
+        num_positive = batch_train_data.filter(F.col(target_column) == True).count()
+        num_negative = batch_train_data.filter(F.col(target_column) == False).count()
+        logger.info(f"Processing Batch {i + 1} / {total_batches} - Size: {batch_size_current}. Proportions in column '{target_column}' -> True: {num_positive}, False: {num_negative} ")
 
         batch_count += batch_size_current
-        # Extract features and labels from the Pandas DataFrame
+
         data_pandas = batch_train_data.toPandas()
         y_train = data_pandas[target_column].values
         X_train = data_pandas[predictors].values
-        dtrain = xgb.DMatrix(X_train, label=y_train, nthread=1)
+        dtrain = xgb.DMatrix(data=X_train, label=y_train, nthread=1)
 
         if i == 0:
-            xgb_model = xgb.train(params, dtrain, num_boost_round=10)
+            xgb_model = xgb.train(params=params, dtrain=dtrain, num_boost_round=10)
         else:
-            xgb_model = xgb.train(params, dtrain, num_boost_round=10, xgb_model=xgb_model)
+            xgb_model = xgb.train(params=params, dtrain=dtrain, num_boost_round=10, xgb_model=xgb_model)
 
         batch_completion_time = time.time() - batch_start_time
-        logger.debug(
-            f"Batch {i + 1} trained. Batch Completion Time: {batch_completion_time / 60:.2f} minutes. {remaining_batches} batches remaining. Percentage of dataset processed: {(batch_count / num_rows) * 100:.2f}%.")
+        logger.info(f"Batch {i + 1} / {total_batches} trained. {remaining_batches} batches remaining. Batch Completion Time: {batch_completion_time / 60:.2f} minutes. Percentage of dataset processed: {(batch_count / num_rows) * 100:.2f}%.")
 
         # Release the memory
         batch_train_data.unpersist()
-        del data_pandas, dtrain, X_train, y_train
+        del data_pandas, batch_train_data, dtrain, X_train, y_train
 
     end_time = time.time() - start_time
-    logger.info(
-        f"XGBoost model training complete. Took: {end_time / 60:.2f} minutes to train {total_batches} batch. Total number of rows processed: {batch_count}.")
+    logger.info(f"XGBoost batch model training complete. Took: {end_time / 60:.2f} minutes to train {total_batches} batch. Total number of rows processed: {batch_count} / {num_rows}.")
 
     model_path = os.path.join(model_dir, experiment_id, "xgb_trained.model")
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
